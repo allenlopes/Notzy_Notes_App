22f70d2a85a206bca40ebea24497fa7d9910babc
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-18 00:17:52 +0530

    In src/components/NoteItem.jsx file modified

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const NoteItem = () => {
+const NoteItem = ({note}) => {
   return (
-    <div>NoteItem</div>
+    <Link to={'/edit-note/${note.id}'} className="note">
+        <h4>{note.title.length > 40 ? (note.title.substr(0, 40)) + '...' : note.title}</h4>
+        <p>{note.date}</p>
+    </Link>
   )
 }
 
