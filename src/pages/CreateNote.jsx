c49832d28aebb241d53fee6cb7d93bb4673cf0fa
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-18 23:59:32 +0530

    In src/pages/CreateNote.jsx file modified

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -3,16 +3,19 @@ import { IoIosArrowBack } from "react-icons/io"
 import { useState } from "react"
 import { v4 as uuid } from 'uuid'
 
+import useCreateDate from "../components/useCreateDate"
+
 
 const CreateNote = () => {
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
+  const date = useCreateDate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if(title && details){
-      const note = {id: uuid(), title, details}
+      const note = {id: uuid(), title, details, date}
       console.log(note);
     }
 
