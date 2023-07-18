6d1f0871da879f2ca215ec5f3d1d2259b728517d
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-18 20:44:53 +0530

    In src/pages/CreateNote.jsx file modified

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -2,18 +2,29 @@ import { Link } from "react-router-dom"
 import { IoIosArrowBack } from "react-icons/io"
 import { useState } from "react"
 
+
 const CreateNote = () => {
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(title && details){
+      console.log(title, details);
+    }
+
+  }
+
   return (
     <section>
       <header className="create-note__header">
         <Link to="/" className="btn"> <IoIosArrowBack /> </Link>
-        <button className="btn lg primary">Save</button>
+        <button className="btn lg primary" onClick={handleSubmit}>Save</button>
       </header>
-      <form className="create-note__form">
+      <form className="create-note__form" onSubmit={handleSubmit}>
         <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} autoFocus/>
-        <textarea rows="28" placeholder="Notes Details..."></textarea>
+        <textarea rows="28" placeholder="Notes Details..." onChange={(e) => setDetails(e.target.value)}></textarea>
       </form>
     </section>
   )
