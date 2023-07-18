13c62ea8825633dddf70d0be7f89f5cac3bbb67f
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-18 18:04:38 +0530

    In src/pages/CreateNote.jsx file modified

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { IoIosArrowBack } from "react-icons/io"
 
 const CreateNote = () => {
   return (
-    <div>CreateNote</div>
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn"> <IoIosArrowBack /> </Link>
+        <button className="btn lg primar">Save</button>
+      </header>
+      <form className="create-note__form">
+        <input type="text" placeholder="Title" />
+        <textarea rows="28" placeholder="Notes Details..."></textarea>
+      </form>
+    </section>
   )
 }
 
