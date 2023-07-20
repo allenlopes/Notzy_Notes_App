1a1450e6e2358d64e065e1b5c176e4c84784c531
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-20 22:44:09 +0530

    In src/pages/Notes.jsx file modified

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -3,18 +3,33 @@ import { Link } from 'react-router-dom'
 import{BsPlusLg} from 'react-icons/bs'
 
 import NoteItem from '../components/NoteItem'
+import { useState } from 'react';
+
 
 const Notes = ({notes}) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [text, setText] = useState('')
+  const [filteredNotes, setFilteredNotes] = useState(notes)
+
+  const handleSearch = () => {
+    setFilteredNotes(notes.filter(note => {
+      if(note.title.toLowerCase().match(text.toLocaleLowerCase())){
+        return note;
+      }
+    }))
+  }
+
   return (
     <section>
       <header className="notes__header">
-        <h2> My Notes</h2>
-        {/* <input type="text" autoFocus placeholder='Keyword...' /> */}
-        <button className='btn'> <AiOutlineFileSearch/> </button>
+        {!showSearch && <h2> My Notes</h2> }
+        {showSearch && <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+        autoFocus placeholder='Keyword...' /> }
+        <button className='btn' onClick={() => setShowSearch(prevState => !prevState)}> <AiOutlineFileSearch/> </button>
       </header>
       <div className="notes__container">
         {
-          notes.map(note => <NoteItem key={note.id} note={note} />)
+          filteredNotes.map(note => <NoteItem key={note.id} note={note} />)
         }
       </div>
       <Link to="/create-note" className='btn add__btn'> <BsPlusLg/> </Link>
